da284a0274bfc552cc6b5c7cbaba63533944f3ae
duodecanol <hwal.choe@vivident.xyz>
2025-04-16 02:11:22 +0900

    SqlAlchemy ULID type by UserDefinedType

--- app/models/types.py
+++ app/models/types.py
@@ -6,11 +6,40 @@ from typing import Literal, Optional, TypeVar, overload
 import ulid
 from sqlalchemy import types, util
 from sqlalchemy.dialects import postgresql
-from sqlalchemy.sql.type_api import Emulated, TypeEngine as TypeEngine
+from sqlalchemy.sql.type_api import Emulated, TypeEngine, UserDefinedType
 from sqlalchemy_utils.types.scalar_coercible import ScalarCoercible
 from ulid import ULID as _python_ULID
 
 _UUID_RETURN = TypeVar("_UUID_RETURN", str, _python_ULID)
+
+
+class _CoerceULIDMixin:
+    @staticmethod
+    def _coerce(value) -> ulid.ULID | None:
+        if not value:
+            return None
+
+        if isinstance(value, str):
+            try:
+                value = ulid.ULID.from_str(value)
+            except (TypeError, ValueError):
+                value = ulid.ULID.from_hex(value)
+
+            return value
+
+        if isinstance(value, int):
+            return ulid.ULID.from_int(value)
+
+        if isinstance(value, bytes):
+            return ulid.ULID.from_bytes(value)
+
+        if isinstance(value, uuid.UUID):
+            return ulid.ULID.from_uuid(value)
+
+        if not isinstance(value, ulid.ULID):
+            return ulid.ULID.from_bytes(value.bytes)
+
+        return value
 
 
 class Ulid(Emulated, TypeEngine[_UUID_RETURN]):
@@ -148,7 +177,42 @@ class Ulid(Emulated, TypeEngine[_UUID_RETURN]):
         return "ulid"
 
 
-class ULIDType(ScalarCoercible, types.TypeDecorator):
+class UserDefinedULIDType(_CoerceULIDMixin, UserDefinedType):
+    cache_ok = True
+
+    def __init__(self):
+        super().__init__()
+
+    def __repr__(self):
+        return super().__repr__()
+
+    def get_col_spec(self, **kw):
+        return "ulid"
+
+    def bind_processor(self, dialect):
+        def process(value) -> str | None:
+            if value is None:
+                return value
+
+            if not isinstance(value, ulid.ULID):
+                value = self._coerce(value)
+
+            return str(value)
+            # return value.bytes
+
+        return process
+
+    def result_processor(self, dialect, coltype):
+        def process(value) -> ulid.ULID | None:
+            if value is None:
+                return value
+
+            return self._coerce(value)
+
+        return process
+
+
+class DifferedULIDType(_CoerceULIDMixin, ScalarCoercible, types.TypeDecorator):
     """
     Stores a ULID in the database as a native UUID column type
     but can use TEXT if needed.
@@ -183,30 +247,6 @@ class ULIDType(ScalarCoercible, types.TypeDecorator):
 
         return dialect.type_descriptor(self.impl)
 
-    @staticmethod
-    def _coerce(value) -> ulid.ULID | None:
-        if not value:
-            return None
-
-        if isinstance(value, str):
-            try:
-                value = ulid.ULID.from_str(value)
-            except (TypeError, ValueError):
-                value = ulid.ULID.from_hex(value)
-
-            return value
-
-        if isinstance(value, bytes):
-            return ulid.ULID.from_bytes(value)
-
-        if isinstance(value, uuid.UUID):
-            return ulid.ULID.from_bytes(value.bytes)
-
-        if not isinstance(value, ulid.ULID):
-            return ulid.ULID.from_bytes(value.bytes)
-
-        return value
-
     def process_bind_param(self, value, dialect) -> bytes | None:
         if value is None:
             return value
@@ -222,3 +262,6 @@ class ULIDType(ScalarCoercible, types.TypeDecorator):
             return value
 
         return self._coerce(value)
+
+
+ULIDType = UserDefinedULIDType
